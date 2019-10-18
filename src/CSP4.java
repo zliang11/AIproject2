a752a7c78d6df5703ea6a5682ab948216ea9891c
jp190 <jeongmin220@gmail.com>
2019-10-17 20:16:54 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/BackTrackingSearchSolver.java

---
+++ src/CSP4.java
@@ -0,0 +1,15 @@
+public class CSP4 extends CSP {
+    public CSP4(){
+        //variables
+        Variable X = new Variable4("X", )
+        Variable Y = new Variable4("Y", )
+
+        this.variables = new ArrayListSet<Variable>();
+        this.variables.add(X);
+        this.variables.add(Y);
+        this.PrintVariable();
+
+        //constraints
+
+    }
+}
---
+++ src/Variable4.java
@@ -0,0 +1,15 @@
+public class Variable4 extends Variable{
+    public Variable4(String varName, Domain dom) {
+        super(varName, dom);
+    }
+
+    @Override
+    public Domain changeDomain() {
+        return null;
+    }
+
+    @Override
+    public Object assignValue() {
+        return null;
+    }
+}
